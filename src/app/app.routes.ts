bd2039c5f6cbef40193d792895505a4e0e649fb5
Mucco2 <161291343+Mucco2@users.noreply.github.com>
2025-11-07 13:49:44 +0100

    Revert "comit"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,3 @@
 import { Routes } from '@angular/router';
 
-// 1. IMPORTER dine nye komponenter
-import { MarketComponent } from './components/market/market';
-import { LoginComponent } from './login/login';
-import { RegisterComponent as Register} from './register/register';
-import { ProfileComponent as Profile} from './profile/profile';
-import { authGuard } from './guards/auth.guard';
-
-// Du har måske andre imports her, f.eks. MarketComponent
-
-export const routes: Routes = [
-  // 1. NÅR BRUGEREN ÅBNER SIDEN (''), SEND THEM TIL '/login'
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  // 2. TILFØJ de nye ruter
-  { path: 'market', component: MarketComponent, canActivate: [authGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: Register },
-  { path: 'profile', component: Profile, canActivate: [authGuard] }
-  // Sørg for at 'wildcard'-ruten (hvis du har en) er til sidst
-  // { path: '**', redirectTo: '' }
-];
+export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,9 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MarketComponent } from "./components/market/market";
 
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MarketComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
